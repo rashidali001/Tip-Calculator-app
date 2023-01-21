eb897bb9501f0dd244ab8a11b5b2043782ec133f
rashidali001 <rashidestonian@gmail.com>
2023-01-21 04:39:16 +0300

    Tip app

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,25 @@
 let tip_amount_value = document.querySelector('#tip-amount-value');
 let total_amount_value = document.querySelector('#total-amount-value');
 let division = document.querySelectorAll('div.division');
+let percentages = document.querySelectorAll(' div.percentages');
+let custom = document.querySelector('#custom');
 
 tip_amount_value.innerHTML = 0.0;
 total_amount_value.innerHTML = 0.0;
 
-let main = document.querySelector('main');
-let body = document.querySelector('body');
-
-if (window.innerWidth > 800)
+function select(value)
 {
-    body.style.display = none;
+    for (let i = 0; i < percentages.length; i++){
+        percentages[i].classList.remove('selected');
+    }
 
+    percentages[value - 1].classList.add('selected');
+    custom.value = "";
+}
+
+function unselect()
+{
+    for (let i = 0; i < percentages.length; i++){
+        percentages[i].classList.remove('selected');
+    }
 }
